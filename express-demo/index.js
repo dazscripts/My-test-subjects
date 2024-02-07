7b60fae723f2763d0079d762d2343941854565c1
daz <71845855+dazscripts@users.noreply.github.com>
2024-02-07 18:09:24 +0000

    fix

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { get_binary } = require('./ChatGPT/images.js');
+const { get_binary } = require('/ChatGPT/images.js');
 
 app.use(express.json())
 
